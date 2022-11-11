a83b46433329d29f687fc1232020b71f9f4b5e06
LautaroGutierrezR <lautyguty97@gmail.com>
2022-11-11 13:32:31 -0300

    actualizacion

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let listaProductos=[Kit207,KitHilux,KitDuster,KitGoltrend,KitFordKA,KitClio]
+
+let Kit207={
+    Marca:"CJR",
+    Precio:50000,
+    Stock:1000,
+}
+
+let KitHilux={
+    Marca:"CJR",
+    Precio:50250,
+    Stock:1000
+}
+
+let KitDuster={
+    Marca:"CJR",
+    Precio:50500,
+    Stock:1000
+}
+
+let KitGoltrend={
+    Marca:"CJR",
+    Precio:50750,
+    Stock:1000
+}
+
+let KitClio={
+    Marca:"CJR",
+    Precio:51000,
+    Stock:1000,
+}
+let KitFordKA={
+    Marca:"CJR",
+    Precio:51250,
+    Stock:1000,
+}
